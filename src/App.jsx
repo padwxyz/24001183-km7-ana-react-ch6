55cd87463d997f81a13f2a35c617f5c622a31af4
Ananda Darma <anandadarma2612@gmail.com>
2024-11-08 22:12:18 +0800

    feat: fetch data from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -7,8 +9,36 @@ import Navbar from "./components/navbar/navbar";
 import HoverButton from "./components/button/HoverButton";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // store data
+  const [shops, setShops] = useState([]);
+  const [error, setError] = useState(null);
+
   const listMenu = ["Home", "About", "Logout"];
+
+  console.log(shops);
+
+  useEffect(() => {
+    const fetchShops = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/api/v1/shops");
+        console.log(response);
+
+        const data = response.data;
+        if (data.isSuccess) {
+          setShops(data.data.shops);
+        } else {
+          setError("error");
+        }
+      } catch (error) {
+        setError(error.message)
+        console.log(error.message);
+      }
+    };
+
+    fetchShops();
+  }, []);
+
+  console.log(shops);
 
   return (
     <>
@@ -36,31 +66,41 @@ function App() {
         </button>
       </header>
 
-      <section className="max-w-6xl mx-auto mt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {" "}
-        {[...Array(4)].map((_, index) => (
-          <div key={index} className="p-4 border rounded-md bg-white shadow-md">
-            {" "}
-            <img
-              src="/path-to-image/car.png"
-              alt="Car"
-              className="w-full h-40 object-cover mb-4"
-            />{" "}
-            <h3 className="font-semibold">Nama/Tipe Mobil</h3>{" "}
-            <p className="text-green-500 font-bold">Rp 430.000 / hari</p>{" "}
-            <p className="text-gray-600 mt-2 text-sm">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </p>{" "}
-            <div className="flex items-center justify-between text-gray-500 text-sm mt-4">
-              {" "}
-              <span>4 orang</span> <span>Manual</span> <span>Tahun 2020</span>{" "}
-            </div>{" "}
-            <button className="w-full px-4 py-2 mt-4 text-white bg-green-500 rounded-md">
-              Pilih Mobil
-            </button>{" "}
-          </div>
-        ))}{" "}
-      </section>
+      <main className="text-center">
+      {/* {loading && <p className></p> } */}
+      {error && <p className="text-red-500">{error}</p> }
+      {!error && (
+        <section className="max-w-6xl mx-auto mt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {shops.map((shop, index) => (
+            <div
+              key={index}
+              className="p-4 border rounded-md bg-white shadow-md"
+            >
+              <img
+                src={shop.products[0].images[0]}
+                alt={shop.products[0].name}
+                className="w-full h-40 object-cover mb-4"
+              />
+              <h3 className="font-semibold">{shop.products[0].name}</h3>
+              <p className="text-green-500 font-bold">
+                {shop.products[0].price}
+              </p>
+              <p className="text-gray-600 mt-2 text-sm">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p>
+              <div className="flex items-center justify-between text-gray-500 text-sm mt-4">
+                <span>4 orang</span> <span>Manual</span> <span>Tahun 2020</span>
+              </div>
+              <button className="w-full px-4 py-2 mt-4 text-white bg-green-500 rounded-md">
+                Pilih Mobil
+              </button>
+            </div>
+          ))}
+        </section>
+      )}
+      </main>
+
+      
 
       {/* <Navbar />
       <NavbarWithStyling
